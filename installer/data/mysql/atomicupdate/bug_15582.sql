a3e43c0ba959a432fc125a270e47f1fdd1f507c0
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2016-01-14 15:03:27 +0000

    Bug 15582: DB changes - add new pref OPACFineNoRenewalsBlockAutoRenew
    Sponsored-by: University of the Arts London

---
+++ installer/data/mysql/atomicupdate/bug_15582.sql
@@ -0,0 +1,3 @@
+INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` )
+VALUES ('OPACFineNoRenewalsBlockAutoRenew','0','','Block/Allow auto renewals if the patron owe more than OPACFineNoRenewals','YesNo')
+
