6bb79d45efb02f63735d44f7502796f9938f8d21
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2016-04-18 16:25:49 +0100

    Bug 15705: Add is_tt param to the new letter

--- installer/data/mysql/atomicupdate/bug_XXXXX.sql
+++ installer/data/mysql/atomicupdate/bug_XXXXX.sql
@@ -1,7 +1,7 @@
 ALTER TABLE issues ADD COLUMN auto_renew_error VARCHAR(16) DEFAULT NULL AFTER auto_renew;
 ALTER TABLE old_issues ADD COLUMN auto_renew_error VARCHAR(16) DEFAULT NULL AFTER auto_renew;
 
-INSERT INTO letter (module, code, name, title, content, message_transport_type) VALUES ('circulation', 'AUTO_RENEWALS', 'notification on auto renewing', 'Auto renewals', 
+INSERT INTO letter (module, code, name, title, content, message_transport_type, is_tt) VALUES ('circulation', 'AUTO_RENEWALS', 'notification on auto renewing', 'Auto renewals',
 "Dear [% borrower.firstname %] [% borrower.surname %],
 [% IF checkout.auto_renew_error %]
 The following item [% biblio.title %] has not been correctly renewed
@@ -20,4 +20,4 @@ You have too much unpaid fines.
 [% END %]
 [% ELSE %]
 The following item [% biblio.title %] as correctly been renewed.
-[% END %]", 'email');
+[% END %]", 'email', 1);
